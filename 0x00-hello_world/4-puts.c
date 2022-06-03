ae7cfe0e41b1c130346f14fd1732d28eb20b7d33
Dare <deeprince2020@gmail.com>
2022-06-02 21:11:44 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 
 /**
  * main: The program entry
- *
- * Description: prints phrases "Programming is like 
+ * Description: prints phrases "Programming is like
  * building a multilingual puzzle" to the terminal
- *
  * Return: 0 if properly exited, non-zero otherwise
  */
 int main(void)
