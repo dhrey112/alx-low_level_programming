38b06ccbc7a094b694e522a12e43717790d09ba0
Dare <deeprince2020@gmail.com>
2022-06-02 21:25:56 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main: The program entry
+ * main - The program entry
+ *
  * Description: prints phrases "Programming is like
  * building a multilingual puzzle" to the terminal
+ *
  * Return: 0 if properly exited, non-zero otherwise
  */
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- *
  *main - Entry point
+ *
+ * description: This a program that print out size
+ * of various data type in C
  *
  *Return: If success, return 0
  */
 int main(void)
 {
-	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
