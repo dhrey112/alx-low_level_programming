5200bcd4ab9673bc8099921d8e1d84c6dac652b2
Dare <deeprince2020@gmail.com>
2022-06-02 21:55:08 -0700

    quote program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print texts without using puts() or printf()
+ *
+ * Return: If success, return 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
