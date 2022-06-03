940f4c943eb947125573527a17489723f49e7322
Dare <deeprince2020@gmail.com>
2022-06-02 20:23:21 -0700

    porogram

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  *main: The program entry
  *
- *Description: prints phrases to the terminal
+ *description: prints phrases to the terminal
  *
  *Return: 0 if properly exited, non-zero otherwise
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
