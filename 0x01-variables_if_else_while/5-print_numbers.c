0ad4d4c658c0ca4d20bd9f74bfd4a037c304ef25
Dare <deeprince2020@gmail.com>
2022-06-03 23:19:15 -0700

    Base 10 Digit printing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ *
+ * Description: Print digit of base 10
+ *
+ * Return: 0, if success
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+}
