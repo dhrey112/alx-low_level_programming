0d151d68064f927d85ef1beb865f472cd016427a
Dare <deeprince2020@gmail.com>
2022-06-08 11:46:07 -0700

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _putchar - print character
+ * main - Entry
  *
  * Description: A program to print _putchar
- * @c: The charater to print
  *
  * Return: 0 if success
  * Otherwise, return -1 for error
@@ -18,7 +17,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('t');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
