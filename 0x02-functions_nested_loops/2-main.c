3342e6239be8e2d02dd7254884d32a504ba62cd5
Dare <deeprince2020@gmail.com>
2022-06-08 19:17:30 -0700

    print lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: main for the print_alphabet_x10 function
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that has no return value
+ *
+ * Description: Print lowercase letters 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int count;
+
+	count = 1;
+	ch = 'a';
+
+	while(count <= 10)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		count++;
+	}
+	_putchar('\n');
+}
+
