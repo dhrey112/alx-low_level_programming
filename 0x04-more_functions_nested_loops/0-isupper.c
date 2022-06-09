0f838c16ee5902b04107b073c56297f7c57b9c43
Dare <deeprince2020@gmail.com>
2022-06-09 07:41:39 -0700

    isuppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Check uppercase
+ * @c: Character to check
+ *
+ * Return: 1 if success; 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef _mn_h_
+#define _main_h_
+
+int _putchar(char);
+int _isupper(int c);
+
+#endif
