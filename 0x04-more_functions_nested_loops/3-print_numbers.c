a5f466b3c58d6768bf741bff19312c83066f1d63
Dare <deeprince2020@gmail.com>
2022-06-09 09:17:12 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number
+ *
+ * Decription: A function that print numbers
+ */
+void print_numbers(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
