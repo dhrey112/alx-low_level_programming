d9743e19441c55c7e98748fa39bf36548a3b6180
Dare <deeprince2020@gmail.com>
2022-06-12 10:45:59 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,36 @@
  */
 int main(void)
 {
-	int i;
-	long int num1, num2, fibNum;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	num1 = 1;
-	num2 = 2;
-	printf("%ld, %ld", num1, num2);
-	for (i = 3; i <= 98; i++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		fibNum = num1 + num2;
-		num1 = num2;
-		num2 = fibNum;
-		printf(", %ld", fibNum);
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
 	printf("\n");
+
 	return (0);
 }
