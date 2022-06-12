4c98cbf86d7bbd19d98e3a24f09f2fccb9df226c
Dare <deeprince2020@gmail.com>
2022-06-12 05:18:33 -0700

    sum of even fibNum

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the first fibonacci number
+ *
+ * Return: Always 0'
+ */
+int main(void)
+{
+	int i;
+	long int num1, num2, fibNum;
+
+	num1 = 1;
+	num2 = 2;
+	printf("%ld, %ld", num1, num2);
+	for (i = 3; i <= 98; i++)
+	{
+		fibNum = num1 + num2;
+		num1 = num2;
+		num2 = fibNum;
+		printf(", %ld", fibNum);
+	}
+	printf("\n");
+	return (0);
+}
